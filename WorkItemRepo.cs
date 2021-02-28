843bbc0d5a8b1a11964ab6b4dff9b6242d21508b
Shahbaz <shabi9@gmail.com>
2021-02-28 14:17:00 +0300

    add di for workitemrepo clas

--- WorkItemRepo.cs
+++ WorkItemRepo.cs
@@ -12,23 +12,23 @@ using WorkItem = Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.WorkIte
 
 namespace WebHooksDevOps
 {
-    public class WorkItemRepo /*: IWorkItemRepo*/
+    public class WorkItemRepo: IWorkItemRepo
     {               
         static WorkItemTrackingHttpClient WitClient;
-        private readonly IOptions<AppSettings> _appSettings;
+        private readonly IOptions<AppSettings> _appSettings;      
 
         public WorkItemRepo(IOptions<AppSettings> appSettings)
         {
             _appSettings = appSettings;
         }
 
-        private static void connectToTFS()
+        private void ConnectToTFS()
         {
             try
             {
                 // Uri baseUri = new Uri("");
                 string baseUri = _appSettings.Value.AzureDevOpsOrgUrl;
-                string pat = "jcs7a2xjaytizhlijntwfiwm6sgtp5uto4ppmw7bz67tcnm4cpna";
+                string pat = _appSettings.Value.AzureDevOpsToken;
                 //string projectName = "";               
                 VssConnection connection = new VssConnection(new Uri(baseUri), new VssBasicCredential(string.Empty, pat));
                 WitClient = connection.GetClient<WorkItemTrackingHttpClient>();               
@@ -40,11 +40,11 @@ namespace WebHooksDevOps
 
         }
 
-        public static async Task<WorkItem> CreateWorkItem(JsonPatchDocument patchDocument, CreateWorkItemModel vm)
+        public  async Task<WorkItem> CreateWorkItem(JsonPatchDocument patchDocument, CreateWorkItemModel vm)
         {
             try
             {
-                connectToTFS();
+                ConnectToTFS();
                 string projectName = vm.Resource.Revision.Fields.SystemTeamProject.ToString();
                 return await WitClient.CreateWorkItemAsync(patchDocument, projectName, "Epic");
             }
@@ -54,11 +54,13 @@ namespace WebHooksDevOps
             }
 
         }
-        public static async Task<WorkItem> UpdateWorkItem(JsonPatchDocument patchDocument, UpdateWorkItemModel vm)
+
+      
+        public async Task<WorkItem> UpdateWorkItem(JsonPatchDocument patchDocument, UpdateWorkItemModel vm)
         {
             try
             {
-                connectToTFS();
+                ConnectToTFS();
                 string projectName = vm.Resource.Revision.Fields.SystemTeamProject.ToString();
                 return await WitClient.UpdateWorkItemAsync(patchDocument, (int)vm.Resource.WorkItemId);
             }
@@ -73,9 +75,10 @@ namespace WebHooksDevOps
     }
 
 
-    //public interface IWorkItemRepo
-    //{
-    //    WorkItem connectToTFS(JsonPatchDocument patchDocument, WorkItemModel vm);
-    //}
+    public interface IWorkItemRepo
+    {
+        Task<WorkItem> CreateWorkItem(JsonPatchDocument patchDocument, CreateWorkItemModel vm);
+        Task<WorkItem> UpdateWorkItem(JsonPatchDocument patchDocument, UpdateWorkItemModel vm);
+    }
 
 }
